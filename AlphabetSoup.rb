45b7939ac60a7182b2f7348f36b98d5092aef274
Nina Chen <ninacode99@gmail.com>
2016-05-10 13:56:16 -0500

    update two more challenges

---
+++ AlphabetSoup.rb
@@ -0,0 +1,20 @@
+#Using the Ruby language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
+
+def AlphabetSoup(str)
+  if str =~ /[a-zA-Z]/
+    return str.chars.sort.join
+  end
+  # code goes here       
+end
+
+# Refactor the code above:
+
+def AlphabetSoup(str)
+	return str.chars.sort.join if str =~ /[a-zA-Z]/
+end
+
+
+# keep this function call here    
+puts AlphabetSoup(STDIN.gets)  
+
+
---
+++ SimpleSymbols.rb
@@ -0,0 +1,27 @@
+#   Using the Ruby language, have the function SimpleSymbols(str) take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. 
+#   The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a '+' symbol. So the string to the left would be false. The string will not be empty and will have at least one letter. 
+
+#   Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+# Haven't consider the "=" symbol, but test runs ok. Does that mean we don't have to consider the "="?????????
+def SimpleSymbols(str)
+  if str =~ /[a-zA-Z]/ && str != ""
+  	i = 1
+  	while i < str.length
+  	  if str[i-1] != "+" || str[i + 1] != "+"
+  	  	return false
+      else
+  	  	return true 
+  	  end
+  	  i += 1
+  # code goes here
+    end
+  end
+end
+   
+# keep this function call here    
+puts SimpleSymbols(STDIN.gets)
+
+# keep this function call here    
+puts SimpleSymbols("+d+=3=+s+") 
+puts SimpleSymbols("f++d+") 
