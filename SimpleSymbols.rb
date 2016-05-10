dd3a7da49640479d707640cb5c6400dd3cc9f750
Nina C <jchenharbin@gmail.com>
2016-05-10 14:00:30 -0500

    Update SimpleSymbols.rb

--- SimpleSymbols.rb
+++ SimpleSymbols.rb
@@ -1,7 +1,10 @@
-#   Using the Ruby language, have the function SimpleSymbols(str) take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. 
-#   The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a '+' symbol. So the string to the left would be false. The string will not be empty and will have at least one letter. 
+# Using the Ruby language, have the function SimpleSymbols(str) take the str parameter being passed 
+# and determine if it is an acceptable sequence by either returning the string true or false. 
+# The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) 
+# and for the string to be true each letter must be surrounded by a '+' symbol. So the string to the left would be false. 
+# The string will not be empty and will have at least one letter. 
 
-#   Use the Parameter Testing feature in the box below to test your code with different arguments.
+
 
 # Haven't consider the "=" symbol, but test runs ok. Does that mean we don't have to consider the "="?????????
 def SimpleSymbols(str)
