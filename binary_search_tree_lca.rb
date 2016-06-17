3fb78583f6cc4ab282ac086a5b0018c32e4f17d0
Nina Chen <ninacode99@gmail.com>
2016-06-17 07:52:55 -0500

    more

--- binary_search_tree_lca.rb
+++ binary_search_tree_lca.rb
@@ -1,6 +1,16 @@
 =begin
-Using the Ruby language, have the function BinarySearchTreeLCA(strArr) take the array of strings stored in strArr, which will contain 3 elements: the first element will be a binary search tree with all unique values in a preorder traversal array, the second and third elements will be two different values, and your goal is to find the lowest common ancestor of these two values. For example: if strArr is ["[10, 5, 1, 7, 40, 50]", "1", "7"] then this tree looks like the following: 
-For the input above, your program should return 5 because that is the value of the node that is the LCA of the two nodes with values 1 and 7. You can assume the two nodes you are searching for in the tree will exist somewhere in the tree.
+Using the Ruby language, have the function BinarySearchTreeLCA(strArr) 
+take the array of strings stored in strArr, which will contain 3 elements: 
+the first element will be a binary search tree with all unique values in a preorder 
+traversal array, the second and third elements will be two different values, 
+and your goal is to find the lowest common ancestor of these two values. 
+For example: if strArr is ["[10, 5, 1, 7, 40, 50]", "1", "7"] then this tree 
+looks like the following: 
+For the input above, your program should return 5 
+because that is the value of the node that is the LCA of the two nodes 
+with values 1 and 7. You can assume the two nodes you are searching 
+for in the tree will exist somewhere in the tree.
+
 1. When the input was ("[3, 2, 1, 12, 4, 5, 13]", "5", "4") your output was incorrect.
 2. When the input was ("[5, 3, 1, 7, 6, 12, 45, 32]", "6", "45") your output was incorrect.
 3. When the input was ("[5, 8, 10, 12, 78, 102]", "102", "12") your output was incorrect.
@@ -8,9 +18,13 @@ For the input above, your program should return 5 because that is the value of t
 
 
 	# first check if the two nums are under the same node. 
-	# if the two num is under the same node, and one of the two nums is less than the number before it, then the number before it in the array should be the LCA, elsif, if the two nums both greater than the number before them, then the LCA is two numbers to the left. 
-	# if the two nums are not under the same node, and is the first two numbers in the array, then the root number is the LCA
-    # if the two nums are not under the same node, and not the first two, return root????
+	# if the two num is under the same node, and one of the two nums is less than 
+  #the number before it, then the number before it in the array should be the LCA, 
+  #elsif, if the two nums both greater than the number before them, then the LCA 
+  #is two numbers to the left. 
+	# if the two nums are not under the same node, and is the first two numbers 
+  #in the array, then the root number is the LCA
+  # if the two nums are not under the same node, and not the first two, return root????
    
    # turn first string into array with integers.
  def binary_search_tree_LCA(strArr)
